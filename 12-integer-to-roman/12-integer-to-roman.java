6fd5e036e998f4938693f54cb23bc5cffdc2f5fb
Fahd Mekawy <40622301+fahdmekawy@users.noreply.github.com>
2022-06-16 22:13:42 +0200

    Time: 9 ms (58.33%), Space: 45.8 MB (43.89%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -1,19 +1,16 @@
-class Solution {
-    public String intToRoman(int num) {
-        
-        System.out.println("Integer: " + num);
-        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] romanLiterals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-
-        StringBuilder roman = new StringBuilder();
-
-        for(int i=0;i<values.length;i++) {
-            while(num >= values[i]) {
-                num -= values[i];
-                roman.append(romanLiterals[i]);
-            }
-        }
-        
-        return roman.toString();
-    }
-}
+ class Solution {
+ 	public String intToRoman(int num) {
+        String[] roman = {"M", "CM","D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV","I"};
+int[] n = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+ 		StringBuilder sb = new StringBuilder();
+ 		int index = 0;
+ 		while(num != 0){
+ 			while(num >= n[index]){
+ 				sb.append(roman[index]);
+ 				num -= n[index];
+ 			}
+ 			index++;
+ 		}
+ 		return sb.toString();
+ 	}
+ }

356956f2d61d36c180c6191c47b8dab6287191f5
Fahd Mekawy <40622301+fahdmekawy@users.noreply.github.com>
2022-06-24 10:39:10 +0200

    Time: 2 ms (72.40%), Space: 54.9 MB (7.85%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length<3) return false;
+        int i = 0 ;
+        for(; i<arr.length-1;i++){
+            if(arr[i] == arr[i+1]){
+                return false;
+            }else if(arr[i]> arr[i+1]){
+                i++;
+                break;
+            }
+        }
+        if(i<2) return false;
+        for(; i<arr.length;i++){
+            if(arr[i-1]<=arr[i]){
+                return false;
+            }
+        }
+        return true;
+     }
+}

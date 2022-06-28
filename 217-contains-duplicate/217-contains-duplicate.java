db7f0dbf8ff02800463583ff2b2ecfc43001bf1f
Fahd Mekawy <40622301+fahdmekawy@users.noreply.github.com>
2022-06-28 13:11:40 +0200

    Time: 6 ms (96.63%), Space: 54.5 MB (92.93%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+   Set<Integer> container = new HashSet<>();
+   for (int i=0; i<nums.length; i++) {
+      if (container.contains(nums[i])) {
+         return true;
+      }
+      container.add(nums[i]);
+   }
+   return false;
+    }
+}

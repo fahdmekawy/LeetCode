afb202e7d11c97be422d0096334ea24a3c3664d3
Fahd Mekawy <40622301+fahdmekawy@users.noreply.github.com>
2025-08-15 16:22:13 +0300

    Time: 1 ms (100%), Space: 149.3 MB (54.05%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.dart
+++ 0228-summary-ranges/0228-summary-ranges.dart
@@ -1,23 +1,17 @@
 class Solution {
   List<String> summaryRanges(List<int> nums) {
-    List<String> result = [];
-    int i = 0;
-    int n = nums.length;
+    final result = <String>[];
+    final n = nums.length;
 
-    while (i < n) {
-      int start = nums[i];
+    for (int i = 0; i < n; i++) {
+      final start = nums[i];
+
       while (i + 1 < n && nums[i + 1] == nums[i] + 1) {
         i++;
       }
-      int end = nums[i];
 
-      if (start == end) {
-        result.add("$start");
-      } else {
-        result.add("$start->$end");
-      }
-
-      i++;
+      final end = nums[i];
+      result.add(start == end ? "$start" : "$start->$end");
     }
 
     return result;
